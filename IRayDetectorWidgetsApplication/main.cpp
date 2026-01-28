41eafd6cecf83a80267221ae2c260cb1097fc001
wangyunfu <wangyunfu1996@126.com>
2026-01-28 09:34:19 +0800

    从RayimDR同步代码；

--- IRayDetectorWidgetsApplication/main.cpp
+++ IRayDetectorWidgetsApplication/main.cpp
@@ -1,37 +1,14 @@
 #include <QtWidgets/QApplication>
 
-#include "IRayDetectorWidgetsApplication.h"
-#include "QtLogger.h"
-
-#include "../IRayDetector/IRayDetector.h"
+#include "../IRayDetector/IRayDetectorWidget.h"
+#include "../IRayDetector/NDT1717MA.h"
+#include "../IRayDetector/QtLogger.h"
 
 int main(int argc, char* argv[])
 {
 	QApplication app(argc, argv);
 	QtLogger::initialize();
-
-	if (DET.Initialize() != 0)
-	{
-		qDebug() << "探测器初始化失败！";
-		DET.DeInitialize();
-	}
-	else
-	{
-		DET.UpdateMode("Mode5");
-		int sw_offset{ -1 };
-		int sw_gain{ -1 };
-		int sw_defect{ -1 };
-		DET.GetCurrentCorrectOption(sw_offset, sw_gain, sw_defect);
-
-		sw_offset = 1;
-		sw_gain = 1;
-		sw_defect = 1;
-		DET.SetCorrectOption(sw_offset, sw_gain, sw_defect);
-
-		DET.SetPreviewImageEnable(0);
-	}
-
-	IRayDetectorWidgetsApplication window;
+	IRayDetectorWidget window;
 	window.show();
 	return app.exec();
 }
